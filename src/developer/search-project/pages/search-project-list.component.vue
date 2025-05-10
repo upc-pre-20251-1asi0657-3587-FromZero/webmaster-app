607a0ab7821c087da453ad09121ae503e0e6d937
Jair Velasquez <106536136+Purak189@users.noreply.github.com>
2025-05-10 15:41:23 -0500

    fix: get all projects and info

--- src/developer/search-project/pages/search-project-list.component.vue
+++ src/developer/search-project/pages/search-project-list.component.vue
@@ -33,16 +33,16 @@ export default{
   },
   created(){
     let id = localStorage.getItem('user id')
-    this.projectService.getAvailableProjects().then((response) => {
+    this.projectService.getAllProjects().then((response) => {
       this.projects = response.map(project => new ProjectEntity({
-        project_ID: project.project_ID,
-        nameProject: project.nameProject,
-        descriptionProject: project.descriptionProject,
-        enterprise_id: project.enterprise_id
+        project_ID: project.id,
+        nameProject: project.name,
+        descriptionProject: project.description,
+        enterprise_id: project.ownerId
       }));
-      this.companyIds = response.map(project => project.enterprise_id);
+      this.companyIds = response.map(project => project.ownerId);
       this.companyIds.forEach(companyId => {
-        this.homeService.getEnterpriseInfoByEnterpriseId(companyId).then( (response) =>{
+        this.homeService.getEnterpriseInfoByID(companyId).then( (response) =>{
           console.log(response)
           this.company.push(new CompanyExplorerEntity(
               response.data.enterprise_id,
