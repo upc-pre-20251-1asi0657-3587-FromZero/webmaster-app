2c0a43583ef5ffe26f841bd66b80f66bf11fffe5
HamesEl <u202212684@upc.edu.pe>
2025-05-11 01:29:22 -0500

    fix(profile): enable editing and persisting all fields except name

--- src/developer/home-developer/components/dev-main-page.component.vue
+++ src/developer/home-developer/components/dev-main-page.component.vue
@@ -1,104 +1,11 @@
-<script>
-import {DeveloperEntity} from "../../../shared/models/developer.model.js";
-import {HomeService} from "../../../../public/services/home.service.js";
-
-export default {
-  name: "developer-page",
-  data() {
-    return {
-      isEditingMain: false,
-      mainText: '',
-      isEditingCategories: [false, false, false, false, false, false],
-      categoryTexts: [],
-      categories: ['categories.country', 'categories.phone', 'categories.email', 'categories.projectsFinished', 'categories.specialties'],
-      value: 0,
-      homeService: new HomeService(),
-      displayDialog: false,
-      newImgUrl: ''
-    };
-  },
-  methods: {
-    toggleEditingMain() {
-      if(this.isEditingMain){
-        console.log(this.developer.id)
-        let updatedInfo ={
-          description: this.mainText,
-          country: this.categoryTexts[0],
-          phone: this.categoryTexts[1],
-          specialties: this.categoryTexts[4],
-          profile_img_url: this.developer.profile_img_url
-        }
-        this.homeService.updateDevInfo(this.developer.id, updatedInfo)
-      }
-      this.isEditingMain = !this.isEditingMain;
-
-    },
-
-    toggleEditingCategory(index) {
-      if(this.isEditingCategories[index]){
-        console.log(this.developer.phone)
-        let updatedInfo ={
-          description: this.mainText,
-          country: this.categoryTexts[0],
-          phone: this.categoryTexts[1],
-          specialties: this.categoryTexts[4]
-        }
-        this.homeService.updateDevInfo(this.developer.id, updatedInfo)
-      }
-      this.isEditingCategories[index] = !this.isEditingCategories[index];
-    },
-    updateImg(){
-      if(this.newImgUrl === ''){
-        return;
-      }
-      let img ={
-        profile_img_url: this.newImgUrl
-      }
-      this.newImgUrl = '';
-      this.homeService.updateDevProfileImg(this.developer.id, img)
-          .then(() => {
-            this.displayDialog = false;
-            window.location.reload();
-          });
-    },
-    openDialog(){
-      this.displayDialog = true;
-    },
-    closeDialog(){
-      this.displayDialog = false;
-    }
-  },
-  components: {},
-  props: {
-    developer: {
-      type: DeveloperEntity,
-      required: true
-    }
-  },
-  created() {
-    console.log("hola", this.developer);
-    this.categoryTexts = [
-      this.developer.country,
-      this.developer.phone,
-      this.developer.user.mail,
-      this.developer.completed_projects,
-      this.developer.specialties
-    ];
-    //this.value = this.developer.rating
-    this.mainText = this.developer.description
-  }
-}
-
-</script>
-
 <template>
   <pv-card aria-label="Developer Information">
     <template #title>
+
       <pv-avatar :image="developer.profile_img_url" class="mr-2" size="xlarge" shape="circle"
                  aria-label="Developer Avatar" @click="openDialog"/>
       <div aria-label="Developer Details">
-        <p>{{ developer.name }}</p>
-        <!--<pv-rating v-model="value" readonly :cancel="false" aria-label="Developer Rating"/>-->
+          <p>{{ developer.name }}</p>
       </div>
     </template>
 
@@ -124,24 +31,125 @@ export default {
           <input v-else v-model="categoryTexts[index]" type="text" class="editable-input"
                  aria-label="Category Text Input"/>
           <pv-button @click="toggleEditingCategory(index)" icon="pi pi-pencil"
-                     class="p-button-rounded p-button-text edit-button" v-if="!isEditingCategories[index] && index !== 2 && index !== 3"
+                     class="p-button-rounded p-button-text edit-button" v-if="!isEditingCategories[index]"
                      aria-label="Edit Category Button"/>
           <pv-button @click="toggleEditingCategory(index)" icon="pi pi-check"
-                     class="p-button-rounded p-button-text edit-button" v-else-if="index !== 2 && index !== 3" aria-label="Confirm Category Button"/>
+                     class="p-button-rounded p-button-text edit-button" v-else aria-label="Confirm Category Button"/>
         </div>
       </template>
-
     </template>
   </pv-card>
 
-  <pv-modal  v-model:visible="this.displayDialog" modal header="Update Image URL" >
+  <pv-modal v-model:visible="this.displayDialog" modal header="Update Image URL">
     <p>Enter the new image URL:</p>
     <input type="text" v-model="newImgUrl" />
     <pv-button label="Accept" @click="updateImg" />
     <pv-button label="Cancel" @click="closeDialog" />
   </pv-modal>
-
 </template>
+
+<script>
+import {DeveloperEntity} from "../../../shared/models/developer.model.js";
+import {HomeService} from "../../../../public/services/home.service.js";
+
+export default {
+  name: "developer-page",
+  data() {
+    return {
+      isEditingMain: false,
+      mainText: '',
+      isEditingCategories: [false, false, false], // Updated to match the number of categories
+      categoryTexts: [],
+      categories: ['categories.country', 'categories.phone', 'categories.specialties'], // Ensure specialties is included
+      value: 0,
+      homeService: new HomeService(),
+      displayDialog: false,
+      newImgUrl: ''
+    };
+  },
+  methods: {
+    async toggleEditingMain() {
+      if (this.isEditingMain) {
+        const updatedInfo = {
+          firstName: this.developer.firstName, // Asegúrate de incluir estos valores
+          lastName: this.developer.lastName,  // Asegúrate de incluir estos valores
+          description: this.mainText,
+          country: this.categoryTexts[0],
+          phone: this.categoryTexts[1],
+          specialties: this.categoryTexts[2],
+          profileImgUrl: this.developer.profile_img_url
+        };
+        await this.homeService.updateDevInfo(this.developer.user.id, updatedInfo);
+      }
+      this.isEditingMain = !this.isEditingMain;
+    },
+
+    async toggleEditingCategory(index) {
+      if (this.isEditingCategories[index]) {
+        const updatedInfo = {
+                  firstName:     this.developer.firstName,    // ¡muy importante!
+                  lastName:      this.developer.lastName,
+                   description:   this.mainText,
+                   country:       this.categoryTexts[0],
+                   phone:         this.categoryTexts[1],
+                   specialties:   this.categoryTexts[4],
+                   profileImgUrl: this.developer.profile_img_url
+             };
+        await this.homeService.updateDevInfo(this.developer.user.id, updatedInfo);
+
+        // Update the local developer object
+        switch (index) {
+          case 0:
+            this.developer.country = this.categoryTexts[0];
+            break;
+          case 1:
+            this.developer.phone = this.categoryTexts[1];
+            break;
+          case 2:
+            this.developer.specialties = this.categoryTexts[2];
+            break;
+        }
+      }
+      this.isEditingCategories[index] = !this.isEditingCategories[index];
+    },
+
+    updateImg() {
+      if (this.newImgUrl === '') {
+        return;
+      }
+      let img = {
+        profile_img_url: this.newImgUrl
+      };
+      this.newImgUrl = '';
+      this.homeService.updateDevProfileImg(this.developer.id, img)
+          .then(() => {
+            this.displayDialog = false;
+            window.location.reload();
+          });
+    },
+    openDialog() {
+      this.displayDialog = true;
+    },
+    closeDialog() {
+      this.displayDialog = false;
+    }
+  },
+  props: {
+    developer: {
+      type: DeveloperEntity,
+      required: true
+    }
+  },
+  created() {
+    this.categoryTexts = [
+      this.developer.country,
+      this.developer.phone,
+      this.developer.specialties // Ensure specialties is included
+    ];
+    this.mainText = this.developer.description;
+  }
+};
+</script>
 
 <style scoped>
 
