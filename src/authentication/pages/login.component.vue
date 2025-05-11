5ae2b28950aa6d58b2c6d954854f0d4ee5b7bb28
ZadoR205 <zaidval@gmail.com>
2025-05-11 11:29:09 -0500

    fix: Login spacing and error message

--- src/authentication/pages/login.component.vue
+++ src/authentication/pages/login.component.vue
@@ -10,42 +10,11 @@ export default {
       authService: new AuthService(),
       users:[],
       loggedId:0,
-      userType:''
+      userType:'',
+      errorMessage:''
     }
   },
-  created() {
-    // localStorage.removeItem('user id');
-    // localStorage.removeItem('user type');
-    // localStorage.removeItem('token');
-    // this.authService.getUsers().then((response) => {
-    //   this.users = response.data;
-    //   console.log(this.users);
-    // });
-  },
   methods:{
-    // handleLogin(userData) {
-    //   // userData contendrá user y password enviados desde el componente LoginCard
-    //   this.user = userData.user;
-    //   this.password = userData.password;
-    //
-    //   console.log('User:', this.user);
-    //   console.log('Password:', this.password);
-    //
-    //   // Iterar sobre los usuarios y comparar credenciales
-    //   for (let i = 0; i < this.users.length; i++) {
-    //     const currentUser = this.users[i];
-    //     if (currentUser.username === this.user && currentUser.password === this.password) {
-    //       console.log("Usuario encontrado");
-    //       this.loggedId = currentUser.id
-    //       this.userType = currentUser.accountType;
-    //       console.log(this.loggedId, this.userType);
-    //       this.navigateToHome();
-    //       return; // Salir del bucle si se encuentra el usuario
-    //     }
-    //   }
-    //   // Si el bucle termina y no se encuentra el usuario
-    //   console.log("Usuario no encontrado");
-    // },
     navigateToHome() {
       this.saveUserToLocalStorage(this.loggedId,this.userType);
       this.$router.push(`/main/${this.userType}/${this.loggedId}`);
@@ -78,6 +47,7 @@ export default {
         this.navigateToHome();
       } catch (error) {
         console.log("Usuario no encontrado");
+        this.errorMessage = "Usuario o contraseña incorrectos";
       }
     },
   }
@@ -86,10 +56,12 @@ export default {
 
 <template>
 
-  <login-card @login="handleLogin"/>
+  <login-card @login="handleLogin" :error-message="errorMessage"/>
 
 </template>
 
 <style scoped>
-
+:deep(body){
+  background-color: #F5F5F5 !important;
+}
 </style>
